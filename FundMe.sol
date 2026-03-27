7ca68d777ddbc7c580328a5839d644ccaae0a6f1
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-03-27 22:44:08 +0000

    Added FundMe contract

---
+++ FundMe.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.6.6 < 0.9.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe{
+
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable{
+        addressToAmountFunded[msg.sender] + msg.value;
+        //more workings and workings
+
+    }
+}
