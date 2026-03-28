508f7e91211401c4456f544b08b87582cd614789
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-03-28 22:27:34 +0000

    working on data feeds

--- FundMe.sol
+++ FundMe.sol
@@ -4,16 +4,26 @@ pragma solidity ^ 0.8.18;
 
 contract FundMe {
 
+    uint256 public minimumUsd = 5;
+
 //function takes and checks to see if sender sneds amount greater than the amount specified
 
     function fund() public payable {
-        require(msg.value > 1e18,"didnt send enough ETH");//1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
+        require(msg.value >= minimumUsd,"didnt send enough ETH");//1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
 
 
     }
 
-    function withdraw() public{
+    function getPrice() public{
+        //Address 
+  
+    }
+
+    function getConversionRate() public{
 
     }
+    
+    }
+
 
 }
