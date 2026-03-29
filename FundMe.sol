35c00cf529fafae20963199d6aceac442062ce19
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-03-29 23:01:10 +0000

    conversionRate

--- FundMe.sol
+++ FundMe.sol
@@ -2,9 +2,10 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from  "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
     uint256 public minimumUsd = 5 * 1e18;
 
@@ -14,7 +15,8 @@ contract FundMe {
 //function takes and checks to see if sender sneds amount greater than the amount specified
 
     function fund() public payable {
-        require(msg.value >= minimumUsd,"didnt send enough ETH");//1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
+        
+        require(msg.value.getConversionRate() >= minimumUsd,"didnt send enough ETH");//1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
 
